6176399d3f426d48be119b0db8102a63814d2fcd
binay94 <binaya.vicky@gmail.com>
2023-04-17 12:42:29 +0530

    ingestion and transformation

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,49 @@
+import os, sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+
+##initialise data ingestion configuration
+@dataclass
+class DataIngestionconfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
+    
+##create a class for data ingestion
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionconfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info('Data ingestion method starts')
+        try:
+            df=pd.read_csv(os.path.join('notebook/data','gems_stone.csv'))
+            logging.info('dataset read as pandas dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info('train-test-split')
+            train_set,test_set=train_test_split(df,test_size=0.30)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('data ingestion completed')
+
+            return(self.ingestion_config.train_data_path,
+                   self.ingestion_config.test_data_path)
+
+        except Exception as e:
+            logging.info('Exception Error occured at data ingestion')
+            raise CustomException(e,sys)
+
+##running data ingestion
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
